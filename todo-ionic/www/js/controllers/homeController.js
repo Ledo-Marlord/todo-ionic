00b86577a64ea7ff84380c0618ed50225a3f59c4
Greg <harrison.greg.c@gmail.com>
2015-05-26 20:29:57 -0400

    Updates for homepage

--- todo-ionic/www/js/controllers/homeController.js
+++ todo-ionic/www/js/controllers/homeController.js
@@ -24,19 +24,20 @@
         };
 
         vm.clearCompleted = function () {
-            var count = 0;
-            var deleted = '';
+            var count = 0;                                          // Set the Counter to 0
+            var deleted = '';                                       // Set the Deleted List to null
 
-            for(var index = 0; index < vm.lists.length; index++) {
-                if (vm.lists[index].completed) {
-                    count++;
-                    deleted = vm.lists.splice(index, 1);
-                    vm.lists = deleted.concat(vm.lists);
-                }
+            // Move items to delete to the beginning of the array
+            for(var index = 0; index < vm.lists.length; index++) {  // For Loop. Loops until index equals vm.lists length
+                if (vm.lists[index].completed) {                    // If the item in Lists has Completed = True
+                    count++;                                            // Add 1 to the counter value
+                    deleted = vm.lists.splice(index, 1);                // Set the current value of Deleted to the value that must be removed
+                    vm.lists = deleted.concat(vm.lists);                // Concat creates a new array from the vm.lists and adds to the end of Deleted
+                }                                                   // Expected Result: [0, 1, 1, 0, 1] -> [ 0, 0, 1, 1, 1]
             }
-            $q.all(vm.lists).then(function() {
-                vm.lists.splice(0, count);
-                vm.$broadcast('scroll.refreshComplete');
+            $q.all(vm.lists).then(function() {                      // Once the For Loop is done.
+                vm.lists.splice(0, count);                          // vm.lists is spliced from the beginning of the array: [0,0,1,1,1]->[1,1,1]
+                vm.$broadcast('scroll.refreshComplete');            // Pull to refresh is returned to disabled
             });
         };
     }
